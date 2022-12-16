a36b5408ef3510858dae62aa26c330d382407b43
Anton Maksimov <anton.maximov@gmail.com>
2022-12-16 14:29:05 +0300

    Initial Commit

---
+++ DroneTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DroneTest
+//
+//  Created by Anton Maksimov on 16.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DroneTest/DroneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroneTestApp.swift
+//  DroneTest
+//
+//  Created by Anton Maksimov on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DroneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
